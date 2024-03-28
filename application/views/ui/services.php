1e65958c3af90cb9efbf8d804388a2c674acc116
[qasimaliattari] <[qasimaly18@gmail.com]>
2024-03-28 14:57:50 +0500

    Add frontend file for PeaceOfMind website

---
+++ application/views/ui/services.php
@@ -0,0 +1,47 @@
+<?php $this->load->view('ui/header') ?>
+<main id="main">
+    <section>
+      <div class="container" data-aos="fade-up">
+        <div class="row">
+          <div class="col-12 text-center mb-5">
+            <div class="row justify-content-center">
+              <div class="col-lg-6">
+                <h2 class="display-4">Our Services</h2>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Educational Articles</h4>
+            <p> Informative and insightful articles covering various aspects of Islam, sports, health, life improvement, and history, providing valuable knowledge and perspectives to readers.</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Sports Coverage</h4>
+            <p>Comprehensive coverage of sports news, analysis, and updates on popular sports events, teams, players, and trends, catering to sports enthusiasts and fans.</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Health and Wellness Tips</h4>
+            <p> Practical tips, guides, and advice on maintaining a healthy lifestyle, including fitness routines, nutrition, mental well-being, preventive healthcare measures, and holistic wellness approaches.</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Life Improvement Resources</h4>
+            <p>Resources, tools, and strategies for personal development, productivity enhancement, goal setting, time management, self-improvement techniques, and positive mindset cultivation.</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Historical Insights</h4>
+            <p> Engaging articles, narratives, and stories exploring historical events, figures, civilizations, cultural heritage, and significant milestones, offering intriguing insights into the past.</p>
+          </div>
+          <div class="col-lg-4 text-center mb-5">
+            <img src="<?= base_url()?>public/ui/assets/img/" alt="" class="img-fluid rounded-circle w-50 mb-4">
+            <h4>Community Engagement Forums</h4>
+            <p> Engage with our vibrant online community through forums and discussion boards, where readers can share experiences, ask questions, exchange ideas, and connect with like-minded individuals passionate about Islam, sports, health, life improvement, and history.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+</main><!-- End #main -->
+<?php $this->load->view('ui/footer') ?>
