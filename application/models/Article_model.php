16aeb46679316fca7048f1968b4904922722232a
qasimali <qasimaly18@gmail.com>
2024-07-02 23:11:47 +0500

    "Formatted Article_model.php: added whitespace, removed trailing newline"

--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -1,28 +1,31 @@
-<?php 
-class Article_model extends CI_Model{
-    public function create($formArray){
-        $this->db->insert('articles',$formArray);
+<?php
+class Article_model extends CI_Model
+{
+    public function create($formArray)
+    {
+        $this->db->insert('articles', $formArray);
         return $this->db->insert_id();
     }
-    public function getarticles(){
-        
-       $result = $this->db->get('articles')->result_array();
-       return $result;
+    public function getarticles()
+    {
+
+        $result = $this->db->get('articles')->result_array();
+        return $result;
     }
-    public function getArticle($id){
-        $this->db->where('id',$id);
+    public function getArticle($id)
+    {
+        $this->db->where('id', $id);
         $res = $this->db->get('articles')->row_array();
-        return $res; 
+        return $res;
     }
-    public function update($id, $formArray) {
+    public function update($id, $formArray)
+    {
         $this->db->where('id', $id);
         $this->db->update('articles', $formArray);
     }
-    public function delete($id) {
+    public function delete($id)
+    {
         $this->db->where('id', $id);
         $this->db->delete('articles');
     }
 }
-
-	
-?>
